4506f8669be57846eafa7ec7a2cc60b6ae07d716
Arvind Achari <66430396+arvindkuruvi@users.noreply.github.com>
2021-02-25 11:16:51 +0530

    Update Commonutil.java

--- src/main/java/com/mindweaver/util/Commonutil.java
+++ src/main/java/com/mindweaver/util/Commonutil.java
@@ -2,9 +2,9 @@ package com.mindweaver.util;
 
 public class Commonutil {
 	//email id to send email
-	public static final String FROM_EMAIL_ID = "mwllpsales@gmail.com";
+	public static final String FROM_EMAIL_ID = "example@gmail.com";
 
 	//password for login email and send email								
-	public static final String PASSWORD = "M1dnGma%5456";
+	public static final String PASSWORD = "example%5456";
 	
 }
